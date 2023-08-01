d54906b76887f1a6daac294d6f081e1126046ec5
wkym461a <wkym461a@gmail.com>
2023-08-01 10:49:47 +0900

    [Change] counter on the default page of vite + react to data management by redux

---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+
+import { counterSlice } from '~/slices/counter';
+
+export const store = configureStore({
+	reducer: {
+		counter: counterSlice.reducer,
+	},
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
