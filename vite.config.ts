892ef409388bc4bbce7f307006a321bf0c287dbb
wkym461a <wkym461a@gmail.com>
2023-08-01 09:39:39 +0900

    [Add] base config in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES
+    ? "cycle-combo-dev"
+    : "./",
   plugins: [react()],
 })
