51b069ad1dfebee1069830b13ca001fa84b7eaaa
wkym461a <wkym461a@gmail.com>
2025-09-06 22:24:16 +0900

    [Fix] タイマー制御ボタンの配置とサイズを修正/タイマーの時間と試合組をより大きく表示

--- src/pages/Home/components/MatchTimer.tsx
+++ src/pages/Home/components/MatchTimer.tsx
@@ -14,13 +14,8 @@ type Props = {
 	isDrawerOpened: boolean,
 }
 
-const CONTENT_WIDTH = 120;
+const CONTENT_WIDTH = 160;
 const CONTENT_HEIGHT = 120;
-
-const PORTRAIT_CTRL_HEIGHT = 60;
-const LANDSCAPE_CTRL_WIDTH = 144;
-
-let isLandscape = true;
 
 function MatchTimer({ isDrawerOpened }: Props) {
 	const { timer_s, isRunning, startTimer, stopTimer, resetTimer } = useTimer();
@@ -38,14 +33,6 @@ function MatchTimer({ isDrawerOpened }: Props) {
 			const rect = ref.current?.getBoundingClientRect();
 			let baseWidth = rect!.width;
 			let baseHeight = rect!.height;
-			// isLandscape = (baseWidth >= baseHeight);
-			isLandscape = false;
-
-			if (isLandscape) {
-				baseWidth -= LANDSCAPE_CTRL_WIDTH;
-			} else {
-				baseHeight -= PORTRAIT_CTRL_HEIGHT;
-			}
 
 			const width = baseWidth ?? CONTENT_WIDTH;
 			const height = baseHeight ?? CONTENT_HEIGHT;
@@ -78,15 +65,13 @@ function MatchTimer({ isDrawerOpened }: Props) {
 
 	const contentWidth = CONTENT_WIDTH * scale;
 	const contentHeight = CONTENT_HEIGHT * scale;
-	const widthOffset = (isLandscape) ? LANDSCAPE_CTRL_WIDTH : 0;
-	const heightOffset = (isLandscape) ? 0 : PORTRAIT_CTRL_HEIGHT;
 	const scaleStyle: CSSProperties = {
 		top: '50%',
 		left: '50%',
 		transform: `translate(-50%, -50%)`,
 		transformOrigin: 'top left',
-		width: `${contentWidth + widthOffset}px`,
-		height: `${contentHeight + heightOffset}px`,
+		width: `${contentWidth}px`,
+		height: `${contentHeight}px`,
 	}
 
 	function handleModalOpen() {
@@ -105,101 +90,47 @@ function MatchTimer({ isDrawerOpened }: Props) {
 			ref={ref}
 			width={(isDrawerOpened) ? 'calc(100% - 300px)' : '100%'}
 			height='100%'
-			sx={{ overflow: 'hidden' }}
+			sx={{ overflow: 'hidden', backgroundColor: 'red' }}
 		>
-			<Box sx={{ ...scaleStyle, position: 'relative' }}>
-				<Stack direction={(isLandscape) ? 'row' : 'column'}>
+			<Box
+				sx={{
+					...scaleStyle,
+					position: 'relative',
+					display: 'inline-flex',
+				}}
+			>
+				<Stack
+					spacing={2}
+					pb={2}
+					sx={{
+						top: 0,
+						left: 0,
+						bottom: 0,
+						right: 0,
+						position: 'absolute',
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center',
+						backgroundColor: 'green',
+					}}
+				>
 					{/* タイマー */}
-					<Box
-						sx={{
-							width: `${(isLandscape) ? contentHeight : contentWidth}px`,
-							height: `${(isLandscape) ? contentHeight : contentWidth}px`,
-							position: 'relative',
-							display: 'inline-flex',
-						}}
+					<Typograpy
+						variant='h1'
+						component='div'
+						fontSize={64 * scale}
+						color={(isRunning) ? 'textPrimary' : 'primary'}
 					>
-						<Stack
-							spacing={4}
-							pb={2}
-							sx={{
-								top: 0,
-								left: 0,
-								bottom: 0,
-								right: 0,
-								position: 'absolute',
-								display: 'flex',
-								alignItems: 'center',
-								justifyContent: 'center',
-							}}
-						>
-							<Typograpy
-								variant='h1'
-								component='div'
-								fontSize={48 * scale}
-								color={(isRunning) ? 'textPrimary' : 'primary'}
-							>
-								{`${min}:${sec}`}
-							</Typograpy>
+						{`${min}:${sec}`}
+					</Typograpy>
 
-							{(matches.length > 0) &&
-								<MatchItem match={matches[currentMatchIndex]} isContained={true} scale={scale / 2} />
-							}
-						</Stack>
-					</Box>
+					{(matches.length > 0) &&
+						<MatchItem match={matches[currentMatchIndex]} isContained={true} scale={scale * 0.635} />
+					}
+
 					{/* 再生・一時停止・キャンセルのボタン類 */}
-					{(isLandscape) ?
-					// 横画面時
-					<Box
-						sx={{
-							width: `${LANDSCAPE_CTRL_WIDTH}px`,
-							height: `${contentHeight}px`,
-						}}
-					>
-						<Stack
-							direction='column'
-							spacing={8}
-							p={2}
-							sx={{
-								height: '100%',
-								alignItems: 'stretch',
-								justifyContent: 'center',
-							}}
-						>
-							{(isRunning) ?
-							<Button
-								variant="outlined"
-								onClick={stopTimer}
-							>
-								一時停止
-							</Button>
-							:
-							<Button
-								variant="outlined"
-								onClick={startTimer}
-							>
-								再開
-							</Button>
-							}
-							<Button
-								variant="outlined"
-								onClick={handleModalOpen}
-							>
-								リセット
-							</Button>
-						</Stack>
-					</Box>
-					:
-					// 縦画面時
-					<Box
-						sx={{
-							width: `${contentWidth}px`,
-							height: `${PORTRAIT_CTRL_HEIGHT}px`,
-						}}
-					>
-						<Stack
-							direction='row'
-							spacing={2}
-							p={1}
+					<Box pt={2} sx={{ width: `${contentWidth}px` }} >
+						<Stack direction='row' spacing={1} p={1}
 							sx={{
 								height: '100%',
 								alignItems: 'center',
@@ -207,32 +138,19 @@ function MatchTimer({ isDrawerOpened }: Props) {
 							}}
 						>
 							{(isRunning) ?
-							<Button
-								variant="outlined"
-								sx={{ width: `33%` }}
-								onClick={stopTimer}
-							>
+							<Button variant="outlined" sx={{ width: `45%` }} onClick={stopTimer}>
 								一時停止
 							</Button>
 							:
-							<Button
-								variant="outlined"
-								sx={{ width: `33%` }}
-								onClick={startTimer}
-							>
+							<Button variant="outlined" sx={{ width: `45%` }} onClick={startTimer}>
 								再開
 							</Button>
 							}
-							<Button
-								variant="outlined"
-								sx={{ width: `33%` }}
-								onClick={handleModalOpen}
-							>
+							<Button variant="outlined" sx={{ width: `45%` }} onClick={handleModalOpen}>
 								リセット
 							</Button>
 						</Stack>
 					</Box>
-					}
 				</Stack>
 			</Box>
 
